c731fdbd0253e448b472be68020b0b9171b578e5
Caio Vinicius <grilocaio@hotmail.com>
2025-10-03 18:38:57 -0300

    versao com banco 2

--- src/app/api/agendamentos/route.ts
+++ src/app/api/agendamentos/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server';
 export async function POST(request: Request) {
     try {
         const body = await request.json();
-        console.log(body); // Verifica se os dados do formulário estão chegando
-        // Apenas retorne uma resposta de sucesso sem tocar no banco de dados.
-        return NextResponse.json({ message: 'Conexão OK!' }, { status: 201 });
+
+        // Temporariamente, apenas retorne uma resposta de sucesso
+        return NextResponse.json({ message: 'API respondeu com sucesso!' }, { status: 201 });
     } catch (error) {
         console.error('Erro ao processar agendamento:', error);
         return NextResponse.json({ error: 'Erro no servidor' }, { status: 500 });
