281a911ecfe180380dc1ea3e51be7eaef1361606
Caio Vinicius <grilocaio@hotmail.com>
2025-10-03 16:34:54 -0300

    versao 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import RegistrationForm from './components/RegistrationForm';
-import LoginForm from './components/LoginForm';
-import WelcomePage from './components/WelcomePage';
-import PanelPage from './components/PanelPage';
-import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 
 function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <Routes>
-                    <Route path="/" element={<WelcomePage />} />
-                    <Route path="/register" element={<RegistrationForm />} />
-                    <Route path="/login" element={<LoginForm />} />
-                    <Route path="/panel" element={
-                        <ProtectedRoute>
-                            <PanelPage />
-                        </ProtectedRoute>
-                    } />
-                </Routes>
+
             </div>
         </BrowserRouter>
     );
